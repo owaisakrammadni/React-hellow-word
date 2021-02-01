9ad10f63d69bf8ba06bf707d47f62f9fc29f6b3c
owaisakrammadni <owaisakrammadni@gmail.com>
2021-02-01 15:07:34 +0500

    owais

--- src/index.js
+++ src/index.js
@@ -4,14 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Hi (){
+  return <div> 
+    <center><div className="main">
+      <button><b> <a href="https://https://github.com/owaisakrammadni?tab=repositories">𝔾𝕚𝕥𝕙𝕦𝕓 𝔸𝕔𝕔𝕠𝕦𝕟𝕥</a></b></button>
+  <h1> Owais</h1>
+    <hr></hr>
+    <h1>ＩＮＴＲＯＤＵＣＴＩＯＮ</h1>
+    <hr></hr>
+    <div className="intro">
+      <h3>Ｎａｍｅ :</h3><span>owais Akram</span>
+      <hr></hr>
+      <h3>Ｗｏｒｋ :</h3><span>Develaper</span>
+      <hr></hr>
+      <h3>Ｓｔｕｄｙ :</h3><span>Dars -e- Nizami</span>
+      <hr></hr>
+    </div>
+    <h1>Ｄｉｓｉｎｇ   ｂｙ  </h1>
+    <hr></hr>
+    <marquee><h2> Owais </h2></marquee>
+    <hr></hr>
+    <h3 className="col">ᗯ𝒽𝓐𝓉Ť𝓈Ãⓟ𝔭:   o3492994251</h3> 
+    </div></center>;
+ 
+  </div>
+}
+ReactDOM.render(<Hi/>,document.querySelector('#root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
